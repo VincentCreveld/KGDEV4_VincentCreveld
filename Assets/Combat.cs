431d7ef31f71e083bd1e8e7621f4f71b3d4d5f96
DESKTOP-P1146LV\vince <lollypoop@hotmail.nl>
2018-04-19 22:22:52 +0200

    Initial commit

---
+++ Assets/Combat.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour {
+	public const int maxHealth = 100;
+	public bool destroyOnDeath;
+
+	[SyncVar]
+	public int health = maxHealth;
+
+	//Werkt niet?
+	[SyncVar]
+	public Color meshColor;
+	private MeshRenderer mR;
+
+	private void Awake() {
+		mR = GetComponent<MeshRenderer>();
+		RpcSetColor();
+	}
+
+	private void GetGrade() {
+		meshColor =  new Color(0,1f - (((float)maxHealth - (float)health) / (float)maxHealth), 0);
+	}
+
+	[ClientRpc]
+	private void RpcSetColor() {
+		GetGrade();
+		mR.material.color = meshColor;
+	}
+
+
+	public void TakeDamage(int amount) {
+		if(!isServer)
+			return;
+
+		health -= amount;
+		RpcSetColor();
+		if(health <= 0) {
+			if(destroyOnDeath) {
+				Destroy(gameObject);
+			}
+			else {
+				health = maxHealth;
+				RpcSetColor();
+				// called on the server, will be invoked on the clients
+				RpcRespawn();
+			}
+		}
+	}
+
+	[ClientRpc]
+	void RpcRespawn() {
+		if(isLocalPlayer) {
+			// move back to zero location
+			RpcSetColor();
+			transform.position = Vector3.zero;
+		}
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+	public GameObject bullet;
+
+	public override void OnStartLocalPlayer() {
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+
+	private void Update() {
+		if(!isLocalPlayer)
+			return;
+
+		var x = Input.GetAxis("Horizontal") * 0.1f;
+		var z = Input.GetAxis("Vertical") * 0.1f;
+
+		transform.Translate(x, 0, z);
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			//Debug.Log("Fire!");
+			CmdFire();
+		}
+
+	}
+
+	[Command]
+	private void CmdFire() {
+		GameObject b = Instantiate(bullet, transform.position - transform.forward, Quaternion.identity);
+		b.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+		NetworkServer.Spawn(b);
+
+		Destroy(b, 2f);
+	}
+
+}
